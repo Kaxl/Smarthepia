b7d1ab66c96343a59baf0635e25e44682b2c0f61
Axel Fahy <axel@fahy.net>
2015-12-24 17:27:31 +0100

    Client done, scheduler not working

--- clientTestAF/src/client.scala
+++ clientTestAF/src/client.scala
@@ -1,36 +1,44 @@
-
 import scala.util.{ Failure, Success }
 import scala.concurrent.Future
 import scala.concurrent.ExecutionContext.Implicits.global
 
+import scala.util.parsing.json._
+
 import scala.io.Source._
 import reactivemongo.api._
+import reactivemongo.api.MongoDriver
+import reactivemongo.api.commands.WriteResult
 import reactivemongo.bson._
+import reactivemongo.bson.BSONDocument
 import reactivemongo._
-import spray.json._
+
+import scala.concurrent.duration._
+import scala.concurrent.ExecutionContext
+import ExecutionContext.Implicits.global
+
+import java.util.Timer
+import java.time.Duration
+
 
 /**
- * Case class representing a sensor
+ * @brief Case class representing a sensor
+ *
+ * Did not manage to keep Int type, had to use Double
  */
 case class Sensor(
-  battery: Int,
+  battery: Double,
   controller: String,
-  humidity: Int,
+  humidity: Double,
   location: String,
-  luminance: Int,
+  luminance: Double,
   motion: Boolean,
-  sensor: Int,
+  sensor: Double,
   temperature: Double,
-  updateTime: Long)
+  updateTime: Double)
 
-    //def this(m: Map[String, Any]) {
-    //  this(m.get("battery").asInstanceOf[Int], m.get("controller").asInstanceOf[String],
-    //    m.get("humidity").asInstanceOf[Int], m.get("location").asInstanceOf[String],
-    //    m.get("luminance").asInstanceOf[Int], m.get("motion").asInstanceOf[Boolean],
-    //    m.get("sensor").asInstanceOf[Int], m.get("temperature").asInstanceOf[Int],
-    //    m.get("updateTime").asInstanceOf[Int])
-    //}
-
+/**
+ * @brief Sensor object to write into BSON
+ */
 object Sensor {
   implicit object SensorWriter extends BSONDocumentWriter[Sensor] {
     def write(sensor: Sensor): BSONDocument = BSONDocument(
@@ -62,6 +70,9 @@ object Sensor {
 }
 
 
+/**
+ * @brief REST client
+ */
 object Client {
 
   private final val urlAll = "http://129.194.185.199:5000/sensors/3/all_measures"
@@ -71,59 +82,105 @@ object Client {
 
   val db = connection("SmarthepiaDB")
   val collection = db.collection("sensors")
-  //val db = connection("t")
-  //val collection = db.collection("test")
 
+  /**
+   * @brief HTTP request on the REST server (raspberry) to get information from sensors
+   *
+   * @param url Url of the server
+   *
+   * @return A string contening the data
+   */
   def get(url: String) = scala.io.Source.fromURL(url).mkString
-  //def get(url: String) = scala.io.Source.fromURL(url).getLines.toList
 
 
+  /**
+   * @brief Main function of the client
+   */
   def main(args: Array[String]): Unit = {
+    //Scheduler.schedule(() => println("Do something"), 0L, 5L, TimeUnit.SECONDS)
+    val system = akka.actor.ActorSystem("system")
+    system.scheduler.schedule(0 seconds, 5 seconds){
+      println("Do something")
+    }
+    //system.scheduler.schedule(0 seconds, 5 seconds)(process())
+
+    //object task extends TimerTask {
+    //  var isRunning = false
+
+    //  def run() {
+    //    if (!isRunning) {
+    //      isRunning = true
+    //      process()
+    //      isRunning = false
+    //    }
+    //  }
+    //}
+
+    //new Timer().schedule(task, 0, 1000)
+    //TimerTask task = new TimerSchedulePeriod();
+    //Timer timer = new Timer();
+    //timer.schedule(task, 100, 100);
+
+    //public void run() {
+    //  println("Timer");
+    //}
+  }
+
+  /**
+   * @brief Download the data and write them into MongoDB
+   *
+   * This function is called repeatedly
+   *
+   * @return
+   */
+  def process(): Unit = {
+    // Get the data from the server
     val data = get(urlAll)
-    println(data)
-    val m = scala.util.parsing.json.JSON.parseFull(data)
-    println(m)
-    //val sensor = new Sensor(this(m.get("battery").asInstanceOf[Int], m.get("controller").asInstanceOf[String],
-    //    m.get("humidity").asInstanceOf[Int], m.get("location").asInstanceOf[String],
-    //    m.get("luminance").asInstanceOf[Int], m.get("motion").asInstanceOf[Boolean],
-    //    m.get("sensor").asInstanceOf[Int], m.get("temperature").asInstanceOf[Int],
-    //    m.get("updateTime").asInstanceOf[Int])
-    //)
-    val sensor = Sensor(100, "Pi 4", 21, "A401", 52, false, 3, 24.0, 1450956999)
-    println(sensor)
+
+    // Parse the json string and create a Map[String, Any] containing the data
+    val json: Option[Any] = JSON.parseFull(data)
+    val m: Map[String, Any] = json match {
+      case Some(value) => value.asInstanceOf[Map[String, Any]]
+      case None => null
+    }
+    // Creation of the sensor
+    val sensor = createSensor(m)
+    // Creation of the document
     val document = BSON.write(sensor)
-    val s = BSON.readDocument[Sensor](document)
-    println(s)
 
-    collection.insert(document)
-    //println(document)
-    //val document = BSONDocument(sensor)
-    //val document = BSON.writeDocument(sensor)
-    //println(data(1))
-    //val m = Map(data map {_.
-    //println(j)
-
-    //val sensor = Sensor(data)
-    //println(sensor)
-
-    //val document = BSONDocument(
-    //  "firstName" -> "Stephaneeee",
-    //  "lastName" -> "Godbillon",
-    //  "age" -> 29)
-
-    println("before insert")
+    // Insert the document into MongoDB
     val future = collection.insert(document)
-    println("after insert")
-    //val future = collection.insert(jValue)
-
     future.onComplete {
       case Failure(e) => throw e
       case Success(lastError) => {
         println("successfully inserted document with lastError = " + lastError)
       }
     }
-    //collection.insert(document).map(lastError =>
-    //    println("Mongo LastError: %s".format(lastError)))
-    sys.exit(0)
+    // WTF is the app not exiting ???
+    //sys.exit(0)
+  }
+
+  /**
+   * @brief Creation of a sensor
+   *
+   * Really bad way to create it IMHO...
+   * But since I did not manage to do it another way yet...
+   *
+   * @param data Data in a map
+   *
+   * @return A new sensor containing data
+   */
+  def createSensor(data: Map[String, Any]): Sensor = {
+    val battery = data.getOrElse("battery", 0).asInstanceOf[Double]
+    val controller = data.getOrElse("controller", "").asInstanceOf[String]
+    val humidity = data.getOrElse("humidity", 0).asInstanceOf[Double]
+    val location = data.getOrElse("location", "").asInstanceOf[String]
+    val luminance = data.getOrElse("luminance", 0).asInstanceOf[Double]
+    val motion = data.getOrElse("motion", 0).asInstanceOf[Boolean]
+    val sensor = data.getOrElse("sensor", 0).asInstanceOf[Double]
+    val temperature = data.getOrElse("temperature", 0.0).asInstanceOf[Double]
+    val updateTime = data.getOrElse("updateTime", 0).asInstanceOf[Double]
+
+    Sensor(battery, controller, humidity, location, luminance, motion, sensor, temperature, updateTime)
   }
 }
