6b07713c900e5c2e257c02af0151eedc0c50ee5a
Hohn Rudolf <rudolf.hohn@gmail.com>
2016-01-12 22:28:04 +0100

    Getting temperatures data from REST Server Scala

--- serverNode/public/sensor/sensor.controller.js
+++ serverNode/public/sensor/sensor.controller.js
@@ -6,24 +6,62 @@
         .controller('SensorController', SensorController);
 
 
-    SensorController.$inject = ['$log', 'SensorService'];
+    SensorController.$inject = ['$log', 'SensorService', '$scope', '$filter'];
 
-    function SensorController($log, SensorService) {
+    function SensorController($log, SensorService, $scope, $filter) {
         var vm = this;
 
+        vm.init = init;
         vm.loadTemperatures = loadTemperatures;
         vm.test = test;
+
+        function init() {
+            // $scope.myJson = {
+            //     type: 'line',
+            //     series: [{
+            //         values: []
+            //     }]
+            // };
+        }
 
         function loadTemperatures(sensorId, piId, startDate, endDate) {
             $log.debug('loadTemperatures called');
             SensorService.GetTemperature(sensorId, piId, startDate, endDate)
-            .then(function(temperatures) {
-                vm.temperatures = temperatures;
-            })
+                .then(function(temperatures) {
+                    var tmpValues = [];
+                    var tmpLabels = [];
+                    vm.temperatures = temperatures;
+                    angular.forEach(temperatures, function(temp) {
+                        // $log.info(temp);
+                        tmpValues.push(temp.temperature);
+                        $log.debug($filter('date')(temp.updateTime*1000, "dd/MM/yyyy"));
+                        tmpLabels.push($filter('date')(temp.updateTime*1000, "dd/MM/yyyy"));
+
+                    });
+
+                    $scope.myJson = {
+                        type: 'line',
+                        "scale-x": {
+                            label: {
+                                text: "Temperatures"
+                            },
+                            labels: tmpLabels
+                        },
+                        "scale-y": {
+                            "min-value": 20,
+                            "max-value": 30
+                        },
+                        series: [{
+                            values: tmpValues
+                        }]
+                    };
+                })
         }
 
         function test() {
             $log.info("This is a test");
         }
+
+        init();
     }
 })();
--- serverNode/public/sensor/sensor.module.js
+++ serverNode/public/sensor/sensor.module.js
@@ -1,3 +1,6 @@
 angular
-    .module('app.sensor', [ ])
-    .constant('REST_SERVER', 'http://86.119.33.122:9000');
+    .module('app.sensor', [ 'zingchart-angularjs' ])
+
+    .constant('REST_SERVER', 'http://86.119.33.122:9000')
+
+    ;    
