6998436805a0231f3730363cc44ac48872772ac7
Axel Fahy <axel@fahy.net>
2016-01-13 15:57:09 +0100

    Modification of REST server to enable CORS

--- serverREST/app/controllers/Application.scala
+++ serverREST/app/controllers/Application.scala
@@ -4,7 +4,9 @@ import javax.inject.{Singleton, Inject}
 import play.api.libs.json._
 import play.api.mvc._
 import org.slf4j.{LoggerFactory, Logger}
-
+import play.api.http.HeaderNames
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Future
 
 /*
  * Main application
@@ -24,4 +26,24 @@ class Application @Inject() extends Controller {
     // Message to print on the / page
     Ok("Smarthepia REST server is running...");
   }
+
+  def options(path: String) = CorsAction {
+    Action { request =>
+      Ok.withHeaders(ACCESS_CONTROL_ALLOW_HEADERS -> Seq(AUTHORIZATION, CONTENT_TYPE, "Target-URL").mkString(","))
+    }
+  }
+}
+
+// Adds the CORS header
+case class CorsAction[A](action: Action[A]) extends Action[A] {
+
+  def apply(request: Request[A]): Future[Result] = {
+    action(request).map(result => result.withHeaders(HeaderNames.ACCESS_CONTROL_ALLOW_ORIGIN -> "*",
+    HeaderNames.ALLOW -> "*",
+    HeaderNames.ACCESS_CONTROL_ALLOW_METHODS -> "POST, GET, PUT, DELETE, OPTIONS",
+    HeaderNames.ACCESS_CONTROL_ALLOW_HEADERS -> "Origin, X-Requested-With, Content-Type, Accept, Referer, User-Agent"
+    ))
+  }
+
+  lazy val parser = action.parser
 }
